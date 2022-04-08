a9f6ad27f076f2e02849d4b5cab4e0c2418ccd03
tomas agnese <tagnese1985@gmail.com>
2022-04-08 13:27:15 -0300

    Remove initializer

--- lib/generators/sentrycustomhelper_generator.rb
+++
@@ -1,6 +0,0 @@
-class SentryCustomHelper < Rails::Generators::Base
-  source_root(File.expand_path(File.dirname(__FILE__)))
-  def copy_initializer
-    copy_file 'sentrycustomhelper.rb', 'config/initializers/sentry_custom_helper.rb'
-  end
-end
